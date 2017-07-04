d2906f82157f5fb0114130d75da2f2199f16b64e
Max_Wang <maxwang510@163.com>
2017-07-04 11:58:37 +0800

    添加toolbar

--- app/src/main/java/com/max/helloandroid/ui/view/MainActivity.java
+++ app/src/main/java/com/max/helloandroid/ui/view/MainActivity.java
@@ -1,27 +1,27 @@
 package com.max.helloandroid.ui.view;
 
-import android.os.Bundle;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 
 import com.max.helloandroid.R;
+import com.max.helloandroid.databinding.ActivityMainBinding;
 import com.max.helloandroid.ui.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreateBinding() {
-
+        mainBinding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
     }
 
     @Override
     protected void initView() {
-
+        initToolbar((Toolbar) mainBinding.includeToolbar.findViewById(R.id.toolbar),"主页");
     }
+
+
 
     @Override
     protected void initEvent() {
@@ -31,5 +31,15 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void loadData() {
 
+    }
+
+    private void initToolbar(Toolbar toolbar, String toolbarName) {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_arrow_back);
+            actionBar.setTitle(toolbarName);
+        }
     }
 }
